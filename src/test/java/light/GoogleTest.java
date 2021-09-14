1479b1e9fb77b44e7761ba28f5e876092445ce3a
sk ahmad <ahmad.sk98@gmail.com>
2021-09-15 00:57:55 +0530

    browser change

--- src/test/java/light/GoogleTest.java
+++ src/test/java/light/GoogleTest.java
@@ -5,13 +5,14 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class GoogleTest {
 	@Test
 	public void validate_google_search_bar() throws InterruptedException {
-		System.setProperty("webdriver.opera.driver", "c:/apps/drivers/win/geckodriver.exe");
-		WebDriver driver = new ChromeDriver();
+		System.setProperty("webdriver.gecko.driver", "c:/apps/drivers/win/geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();
 		
 		driver.navigate().to("https://www.google.com/");
 		WebElement searchbar = driver.findElement(By.name("q"));
